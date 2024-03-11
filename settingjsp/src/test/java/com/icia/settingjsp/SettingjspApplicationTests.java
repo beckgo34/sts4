01597f601c51fe40b3b0c4d4ad6f5a1211ff4de4
beckgo34 <beckgo34@naver.com>
2024-03-11 11:23:32 +0900

    setting prectice

---
+++ settingjsp/src/test/java/com/icia/settingjsp/SettingjspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icia.settingjsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SettingjspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
