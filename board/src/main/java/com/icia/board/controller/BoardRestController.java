8ae4cb934a0ef19e9a18ec65e8e014f3ef374272
beckgo34 <beckgo34@naver.com>
2024-03-21 11:20:50 +0900

    logback setting

--- board/src/main/java/com/icia/board/controller/BoardRestController.java
+++ board/src/main/java/com/icia/board/controller/BoardRestController.java
@@ -1,10 +1,14 @@
 package com.icia.board.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.icia.board.service.MemberService;
 
 @RestController
 public class BoardRestController {
-	
+	@Autowired
+	private MemberService mServ;
 	
 	
 	
