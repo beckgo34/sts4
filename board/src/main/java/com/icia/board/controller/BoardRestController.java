939ae8dcb7e8d606ca65185335e39373e216dea8
beckgo34 <beckgo34@naver.com>
2024-03-21 13:27:03 +0900

    idcheck done

--- board/src/main/java/com/icia/board/controller/BoardRestController.java
+++ board/src/main/java/com/icia/board/controller/BoardRestController.java
@@ -1,16 +1,29 @@
 package com.icia.board.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.icia.board.service.MemberService;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
+@Slf4j
 public class BoardRestController {
 	@Autowired
 	private MemberService mServ;
 	
-	
+	// id 중복체크
+	@GetMapping("idCheck")
+	public String idCheck(@RequestParam("mid") String mid) {
+		log.info("idCheck()");
+		
+		String res = mServ.idCheck(mid);
+		
+		return res;
+	}
 	
 	
 	
--- board/src/main/java/com/icia/board/service/MemberService.java
+++ board/src/main/java/com/icia/board/service/MemberService.java
@@ -52,7 +52,22 @@ public class MemberService {
 		return view;
 	}
 	
-	
+	// id 중복체크
+	public String idCheck(String mid) {
+		log.info("idCheck()");
+		
+		String result = null;
+		
+		int memCnt = mDao.selectId(mid);
+		
+		if(memCnt == 0) {
+			result = "ok";
+		}else {
+			result = "fail";
+		}
+		
+		return result;
+	}
 	
 	
 	
