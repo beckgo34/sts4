2ab3f6e6417a0611b2c6893e66c39ad6ec39813c
beckgo34 <beckgo34@naver.com>
2024-03-26 13:13:22 +0900

    reply view

--- board/src/main/java/com/icia/board/dto/ReplyDto.java
+++ board/src/main/java/com/icia/board/dto/ReplyDto.java
@@ -12,11 +12,11 @@ import lombok.Setter;
 public class ReplyDto {
 	private int r_num; // 댓글 번호
 	private int r_bnum; // 게시글 번호
-	private String contents;
+	private String r_contents;
 	private String r_id; // 작성자 id
 	
 	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
-	private Timestamp r_data;
+	private Timestamp r_date;
 }
 // @JsonFormat
 // ajax(RESTful)로 날짜를 전송할 때 날짜 형식을 지정할 수 있다.
